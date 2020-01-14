499feabcc483dda6483077262e20b5a16244a4c0
Naama Dayan <namadayan@gmail.com>
2020-01-14 22:55:04 +0200

    relate to number on cells, add while loop

--- Agent.py
+++ Agent.py
@@ -20,10 +20,13 @@ class Agent(object):
     def __init__(self, n, m, bombs):
         self.location = (0, 0)
         self.board = Board(n, m, bombs)
+        self.max_moves = n * m * 2
+        self.moves = 0
 
     def reset(self, board_num):
         self.location = (0, 0)
         self.board.reset(board_num)
+        self.moves = 0
 
     def move(self, direction):
         temp_loc = tuple(map(sum, zip(self.location, Constants.directions[direction])))
@@ -33,10 +36,10 @@ class Agent(object):
     def reveal(self):
         self.board.reveal(self.location)
 
-    def mark(self):
+    def flag(self):
         self.board.mark(self.location)
 
-    def unmark(self):
+    def unflag(self):
         self.board.unmark(self.location)
 
     def num_bombs(self):
@@ -54,13 +57,14 @@ class Agent(object):
     def flag_all(self):
         self.board.flag_all(self.location)
 
-    def uncover_all(self):
-        self.board.uncover_all(self.location)
+    def reveal_all(self):
+        self.board.reveal_all(self.location)
 
     def run(self, actions, board_num):
         self.reset(board_num)
-        # while not self.board.finished():  # TODO change
-        actions()
+        while self.moves < self.max_moves:
+            actions()
+            self.moves += 1
 
     def if_all_safe(self):
         return self.num_flags() == self.num_bombs()
